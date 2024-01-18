f59c021568a50c0245d10b5b8ca96b5495e191f9
windsonamorim <windson.amorim@gmail.com>
2024-01-17 22:35:18 -0300

    feat: add estilo global

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { AppBarDefault } from "../../components/menu";
+import { CardDefault } from "../../components/card";
+import { CssBaseline } from "@mui/material";
 
 
 export const Home = () => {
   return (
     <>
+      <CssBaseline />
       <AppBarDefault />
+      <CardDefault />
     </>
   );
 };
