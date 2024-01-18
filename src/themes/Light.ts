34a389b317d9bd6b12110f50c8544dd2e48ed396
windsonamorim <windson.amorim@gmail.com>
2024-01-17 22:35:45 -0300

    feat: add estilo global em theme

--- src/themes/Light.ts
+++ src/themes/Light.ts
@@ -1,12 +1,29 @@
-import { createTheme } from "@mui/material";
+import { createTheme, CssBaseline } from "@mui/material";
 
 export const LightTheme = createTheme({
-    palette:{
-        primary:{
-            main:'#560BAD',
-            dark:'#3c0779',
-            light:'#773bbd',
-            contrastText:'#ffffff',
-        }
-    }
-})
+  palette: {
+    primary: {
+      main: "#560BAD",
+      dark: "#3c0779",
+      light: "#773bbd",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#ffffff",
+    },
+    background: {
+      default: "#ffffff",
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        "*": {
+          margin: 0,
+          padding: 0,
+          boxSizing: "border-box",
+        },
+      },
+    },
+  },
+});
